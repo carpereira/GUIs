e9493fe061945dafdda97bc2358f528822c99814
carpereira <59072054+carpereira@users.noreply.github.com>
2025-08-22 18:41:55 -0300

    Update main.c

--- exe-1.3.1/main.c
+++ exe-1.3.1/main.c
@@ -1,1 +1,62 @@
+#include <SDL2/SDL.h>
+
+int main(int argc, char* args[])
+{
+/*INICIALIZAÇÃO*/
+SDL_Init(SDL_INIT_EVERYTHING);
+SDL_Window* win = SDL_CreateWindow("Animação Circular!",
+                      SDL_WINDOWPOS_UNDEFINED,
+                      SDL_WINDOWPOS_UNDEFINED,
+                      400, 400, SDL_WINDOW_SHOWN
+                      );
+SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
+
+/*EXECUÇÃO*/
+SDL_Rect r = {80,20,10,10};
+SDL_Rect s = {320,20,10,10};
+SDL_Rect t = {320,380,10,10};
+SDL_Rect w = {80,380,10,10};
+
+while (r.x<320){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren, &r);
+    SDL_RenderPresent(ren);
+    SDL_Delay(200);
+    r.x += 2;    
+}
+while (s.y<380){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren, &s);
+    SDL_RenderPresent(ren);
+    SDL_Delay(200);
+    s.y += 2;    
+}
+while (t.x>80){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren, &t);
+    SDL_RenderPresent(ren);
+    SDL_Delay(200);
+    t.x -= 2;    
+}
+while (w.y>20){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren, &w);
+    SDL_RenderPresent(ren);
+    SDL_Delay(200);
+    w.y -= 2;    
+}
+ 
+/*FINALIZAÇÃO*/
+SDL_DestroyRenderer(ren);
+SDL_DestroyWindow(win);
+SDL_Quit();
+}
 
