cb0032ab98c1afbc0bf0bbc881254c9aaa4a6494
carpereira <59072054+carpereira@users.noreply.github.com>
2025-08-23 00:29:34 -0300

    Update main.c

--- exe-1.3.1/main.c
+++ exe-1.3.1/main.c
@@ -12,48 +12,53 @@ SDL_Window* win = SDL_CreateWindow("Animação Circular!",
 SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 
 /*EXECUÇÃO*/
-SDL_Rect r = {80,20,10,10};
-SDL_Rect s = {320,20,10,10};
-SDL_Rect t = {320,380,10,10};
-SDL_Rect w = {80,380,10,10};
+SDL_Rect r = {40,20,10,10};
+SDL_Rect s = {360,20,10,10};
+SDL_Rect t = {360,380,10,10};
+SDL_Rect w = {20,380,10,10};
 
-while (r.x<320){
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-    SDL_RenderFillRect(ren, &r);
-    SDL_RenderPresent(ren);
-    SDL_Delay(200);
-    r.x += 2;    
-}
-while (s.y<380){
+while (w.y>20){
+    if(r.x<360){
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
+        SDL_RenderPresent(ren);
+        SDL_Delay(100);
+        r.x += 2;
+        r.y = 20; 
+    } 
+    else if(r.x==360 && s.y<380){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
     SDL_RenderFillRect(ren, &s);
     SDL_RenderPresent(ren);
-    SDL_Delay(200);
-    s.y += 2;    
+    SDL_Delay(100);
+    r.x = 360;
+    s.y += 2;     
 }
-while (t.x>80){
+    else if(s.y==380 && t.x>20){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
     SDL_RenderFillRect(ren, &t);
     SDL_RenderPresent(ren);
-    SDL_Delay(200);
+    SDL_Delay(100);
+    s.y = 380;
     t.x -= 2;    
 }
-while (w.y>20){
+    else if(t.x==20 && w.y>20){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
     SDL_RenderFillRect(ren, &w);
     SDL_RenderPresent(ren);
-    SDL_Delay(200);
+    SDL_Delay(100);
+    t.x = 20;
     w.y -= 2;    
 }
- 
+}
 /*FINALIZAÇÃO*/
 SDL_DestroyRenderer(ren);
 SDL_DestroyWindow(win);
